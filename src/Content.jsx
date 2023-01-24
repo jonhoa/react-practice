b90807b766a9f997f239b7bf09ca11b6b596654d
Jonathan <jonitoadriano@gmail.com>
2023-01-24 12:40:07 -0600

    calender app

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,22 @@
 import { ProductsIndex } from "./ProductsIndex";
 import { ProductsNew } from "./ProductsNew";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { useState } from "react";
+
 
 export function Content() {
+  const [value, setValue] = useState(new Date());
+
+  function onChange(activeStartDate) {
+    setValue(activeStartDate);
+  }
   return (
     <div>
+      <p>content page</p>
       <ProductsNew />
+      <Calendar onChange ={onChange}
+        value = {value}/>
       <ProductsIndex />
     </div>
   );
